54d6cfaf235a16c68cb350ba67412f5ee1da730e
patilpranit88 <166053497+patilpranit88@users.noreply.github.com>
2024-07-21 16:52:30 +0530

    Add files via upload

---
+++ Overloading.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std; 
+
+class complex{
+    int a,b;
+    public:
+    complex(){
+        a=0;
+        b=0;
+    }
+    complex(int x,int y){
+        a=x;
+        b=y;
+    }
+    complex(int x){
+        a=x;
+        b=0;
+    }
+    void printnum()
+    {
+        cout<<"your number is "<<a<<"+"<<b<<"i"<<endl;
+    }
+};
+int main(){
+    complex c1(4,6);
+    c1.printnum();
+    complex c2(5);
+    c2.printnum();
+    complex c3;
+    c3.printnum();
+    
+    return 0;
+}
