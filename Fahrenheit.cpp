2bcba723b478420df4f1d10ba43bd5fa7d17c83b
patilpranit88 <166053497+patilpranit88@users.noreply.github.com>
2024-07-21 16:43:51 +0530

    Add files via upload

---
+++ Fahrenheit.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std; 
+
+int main(){
+    float f,c;
+    cout<<"enter the tempepereture in fahreneit "<<endl;
+    cin>>f;
+    c=(5*(f-32))/9;
+    cout<<"temprature in celcius is ="<<c<<endl;
+    
+    
+    return 0;
+}
